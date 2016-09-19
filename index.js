461979b8e7e873e4a6ec50564a7e56cfc95beddc
EDA <eda@mac-mini-3.local>
2016-09-19 14:47:21 +1200

    nice error

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var bodyParser = require('body-parser')
 var express = require('express')
-var hbs = require('express-handlebars')
 var path = require('path')
 
 var index = require('./routes/index')
 
 var PORT = 3000
-
 var app = express()
-app.engine('hbs', hbs())
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'views'))
-app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
 
 app.get('/', index.get)
+//
+app.get('/:id', index.getUser)
+app.delete('/:id', index.delUser)
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,61 @@
 var express = require('express')
 var development = require('../knexfile').development
 var knex = require('knex')(development)
+var router = express.Router()
 
 module.exports = {
-  get: get
+  get: get,
+  getUser: getUser,
+  delUser: delUser
 }
 
 function get (req, res) {
   knex('users')
     .select()
     .then(function (users) {
-      res.render('index', { users: users })
+      res.json(users)
     })
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
 }
+
+function getUser(req, res) {
+  knex('users')
+  .select()
+  .where( "id", "=", req.params.id)
+  .then(function (users) {
+    res.json(users[0])
+  })
+  .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+}
+
+function delUser (req, res) {
+  knex('users')
+  .del()
+  .where( "id", "=", req.params.id )
+  .then(function (delcount) {
+    if (delcount == 1) {
+      res.send(204)
+    } else {
+      res.send(404)
+    }
+  })
+  .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+}
+
+
+
+// var knex = require("knex")(knexConfig);
+// knex("test")
+// .where("col1","a4")
+// .del()
+// .then(function (count) {
+//   console.log(count);
+// }).finally(function () {
+//   knex.destroy();
+// });
